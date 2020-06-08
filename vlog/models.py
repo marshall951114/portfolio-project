b2f0aa9022b5ab63dfa97fdccef508ebad4a5a3a
Marshall Ma <mashuai951114@gmail.com>
2020-06-08 16:44:45 -0400

    finish

--- vlog/models.py
+++ vlog/models.py
@@ -5,3 +5,6 @@ class Vlog(models.Model):
 	title = models.CharField(max_length=200)
 	body = models.TextField()
 	image = models.ImageField(upload_to='image/')
+
+	def __str__(self):
+		return self.title
